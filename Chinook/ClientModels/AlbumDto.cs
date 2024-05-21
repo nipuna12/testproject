1cb80321b845a5b85a9a4c0d4674934b0d7270e9
Nipuna Danwatte <nipuna.danwatte@cammsgroup.com>
2024-05-22 00:52:00 +0530

    code review changes

---
+++ Chinook/ClientModels/AlbumDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Chinook.ClientModels
+{
+    public class AlbumDto
+    {
+        public long AlbumId { get; set; }
+        public string? Title { get; set; }
+        public long ArtistId { get; set; }
+    }
+}
---
+++ Chinook/ClientModels/ArtistDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Chinook.ClientModels
+{
+    public class ArtistDto
+    {
+        public long ArtistId { get; set; }
+        public string? Name { get; set; }
+        public int AlbumCount { get; set; }
+        public AlbumDto AlbumList { get; set; }
+    }
+}
